6a7ff0d6b3805a923a33e48738b720b89296d6bb
LMeister <111584068+PatZhang0214@users.noreply.github.com>
2023-05-06 20:11:09 -0400

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        boolean found = false;
+        int index = 0;
+        int returnValue[2];
+        HashMap<Integer, Integer> record = new HashMap<Integer, Integer>();
+
+        while(!found) {
+            int remaining = target - nums[index];
+
+            if(record.get(remaining) != null) {
+
+                returnValue[0] = record.get(remaining);
+                returnValue[1] = index;
+
+                return returnValue;
+ 
+            } else {
+                record.put(nums[index], index);
+            }
+            index++;   
+        }
+    }
+}
